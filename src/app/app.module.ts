2c5c751e31680378a27d7fe2c69e548289986348
Scribtor <unocare@really.com>
2020-02-21 00:02:06 +0100

    Dodate 2+2 komponente.Header,nav,Wlist,Wedit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { WineListComponent } from './wine/wine-list/wine-list.component';
+import { EditWineComponent } from './wine/edit-wine/edit-wine.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    WineListComponent,
+    EditWineComponent
   ],
   imports: [
     BrowserModule,
