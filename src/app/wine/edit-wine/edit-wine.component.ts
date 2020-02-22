4b50c839425188644b5f72393e6c428a52ef0617
Scribtor <unocare@really.com>
2020-02-22 22:21:16 +0100

    Dodato rutiranje.Dopunjen obrazac App-router-module

--- src/app/wine/edit-wine/edit-wine.component.ts
+++ src/app/wine/edit-wine/edit-wine.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'wcellar-edit-wine',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditWineComponent implements OnInit {
 
-  constructor() { }
+  constructor(private rt: Router) { }
 
   ngOnInit(): void {
   }
