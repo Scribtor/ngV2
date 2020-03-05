02acdde8cda838e5e38d532741981b9ade23bb19
Scribtor <unocare@really.com>
2020-03-05 11:08:46 +0100

    ngDestroy

--- src/app/wine/wine-list/wine-list.component.ts
+++ src/app/wine/wine-list/wine-list.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Wine } from '../model/wine.model'
 import { ServedWineService } from '../services/served-wine.service'
 import { WineService } from '../services/wine.service'
+import { Subscription } from 'rxjs'
 @Component({
   selector: 'wcellar-wine-list',
   templateUrl: './wine-list.component.html',
   styleUrls: ['./wine-list.component.scss']
 })
 
-export class WineListComponent implements OnInit {
+export class WineListComponent implements OnInit,OnDestroy {
+  
   public poslatLimit:number;
   public ListaVina: Wine[]=[];
   public brojElemenataPoStranici:number=0;
+  public sub:Subscription;
   constructor(private wsL:WineService,private wsH:ServedWineService)
   {
   }
@@ -54,14 +57,22 @@ export class WineListComponent implements OnInit {
     this.izmeniStranuPaginacije(1);
 
   }
+  serveService()
+  {
+    this.ListaVina=this.wsL.vratiSve();
+    this.poslatLimit=this.wsL.krajnjiID;
+  }
   ngOnInit(): void {
-    this.wsH.getData().subscribe
+    this.sub= this.wsH.getData().subscribe
       (
       data => { this.wsL.krajnjiID = data.count; this.wsL.spisak = data.wines;}, 
       error =>{console.log("Error fetching data, because: ", error.statusText);},
-      () => { this.ListaVina= this.wsL.vratiSve(); this.poslatLimit=this.wsL.krajnjiID; }
+      () => { this.serveService() }
       );
       console.log(this.ListaVina);
   }
-
+  ngOnDestroy (): void
+  {
+    this.sub.unsubscribe();
+  }
 }
