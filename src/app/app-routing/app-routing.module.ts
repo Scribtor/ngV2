3dfb7d6b093e893db1c7a6c0f96c1074437fc6a2
Scribtor <unocare@really.com>
2020-03-08 17:34:34 +0100

    prčking1

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -14,4 +14,5 @@ export class AppRoutingModule { }
 // Ovde nema apsolutno nikakvih dešavanja zasad osim što je router modul uključen
 // Ponavljam, ja sam prilikom generisanja aplikacije dao custom argument --routing 
 // Angular je SAM definisao ovaj fajl, gde sam ja dopunio samo sa importom svog routes.ts fajla
-// I smeštanjem ovog fajla i routes.ts u poseban podfolder, i regulisanjem putanja u glavnom fajlu app.module.ts 
+// I smeštanjem ovog fajla i routes.ts u poseban podfolder, i regulisanjem putanja u glavnom fajlu 
+// app.module.ts 
--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -2,12 +2,13 @@ import { Routes } from "@angular/router";
 import { WineListComponent } from '../wine/wine-list/wine-list.component';
 import { EditWineComponent } from '../wine/edit-wine/edit-wine.component';
 
-export const routes: Routes = [
+export const routes: Routes = 
+[
   {path: 'wines', component: WineListComponent},
   {path: 'wines/add', component: EditWineComponent},
   {path: 'wines/:id', component: EditWineComponent},
   {path: '', redirectTo: '/wines', pathMatch: 'full'}
-  ];
+];
 
   // Vrlo rudimentaran fajl
   // Importovan je ruter, kao i komponente koje podležu rutiranju
